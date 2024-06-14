bdc9767754027f4363b11e7fc2dabd904066b7d6
Hamza Nisar <hamzanisar@Hamza-Nisar.local>
2024-06-14 13:13:10 +0500

    aligned everything:

---
+++ projects/project1/app/Providers/CustomResponseServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
+
+class CustomResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Response::macro('success', function ($data = null, $message = 'Success', $statusCode = 200) {
+            return response()->json([
+                'success' => true,
+                'message' => $message,
+                'data' => $data
+            ], $statusCode);
+        });
+
+        Response::macro('error', function ($message = 'Error', $statusCode = 500) {
+            return response()->json([
+                'success' => false,
+                'message' => $message
+            ], $statusCode);
+        });
+
+        Response::macro('apiResponse', function ($apiToken, $user) {
+            return response()->json([
+                'token' => $apiToken->token,
+                'user' => $user
+            ]);
+        });
+    }
+}
