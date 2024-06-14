315181b55584e3f504ded0f5743dea3d65f64bec
Awais Akram <awaisakram@Awais-Akram.local>
2024-06-14 15:44:22 +0500

    Multiple Routes files created

---
+++ projects/project2/routes/auth/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+
+Route::post('/register', [AuthController::class, 'register'])->middleware('validation:register');
+Route::get('/register/confirm/{token}', [AuthController::class, 'confirmEmail'])->name('register.confirm');
+Route::post('/login', [AuthController::class, 'login'])->middleware('validation:login');
+Route::post('/logout', [AuthController::class, 'logout']);
